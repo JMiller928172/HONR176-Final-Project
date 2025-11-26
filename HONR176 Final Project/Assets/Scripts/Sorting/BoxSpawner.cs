24574639461d4a1d90e9c4f9e42547f80c33de99
JMiller921387 <tyfsak61@outlook.com>
2025-11-26 00:26:00 -0500

    Changed BoxSpawner for individual spawning

--- HONR176 Final Project/Assets/Scripts/Sorting/BoxSpawner.cs
+++ HONR176 Final Project/Assets/Scripts/Sorting/BoxSpawner.cs
@@ -1,25 +1,51 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BoxSpawner : MonoBehaviour
 {
-    public GameObject boxPrefab; 
-    public RectTransform parentCanvas; 
-    public float spacing = 100f;   
+    public GameObject boxPrefab;
+    public RectTransform parentCanvas;
+    public int totalBoxes = 10;
+    public float startY = -50f;
+
+    private int spawnedCount = 0;
+    private List<GameObject> activeBoxes = new List<GameObject>();
 
     void Start()
     {
-        SpawnBoxes();
+        SpawnNextBox();
     }
 
-    void SpawnBoxes()
+    void Update()
     {
-        for (int i = 0; i < 10; i++)
+        if (AllBoxesInSlots() && spawnedCount < totalBoxes)
         {
-            
-            GameObject newBox = Instantiate(boxPrefab, parentCanvas);
-
-            RectTransform rt = newBox.GetComponent<RectTransform>();
-            rt.anchoredPosition = new Vector2(i * spacing, -50f); 
+            SpawnNextBox();
         }
+    }
+
+    void SpawnNextBox()
+    {
+        GameObject newBox = Instantiate(boxPrefab, parentCanvas);
+
+        RectTransform rt = newBox.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector2(0, startY);
+
+        activeBoxes.Add(newBox);
+        spawnedCount++;
+    }
+
+    bool AllBoxesInSlots()
+    {
+        foreach (GameObject box in activeBoxes)
+        {
+            if (box == null) continue; 
+
+            if (box.transform.parent.GetComponent<Slot>() == null)
+            {
+                return false; 
+            }
+        }
+        return true;
     }
 }
